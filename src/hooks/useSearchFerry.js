8f7b277563daf709028f8f1e1f5409ae2562cfd9
Julianmenav <julianmenav2@gmail.com>
2023-09-28 20:47:34 +0200

    refactor: Change useSearchFerry to async await

--- src/hooks/useSearchFerry.js
+++ src/hooks/useSearchFerry.js
@@ -1,55 +1,55 @@
-import { useState } from "preact/hooks"
-
+import { useState } from "preact/hooks";
 
 export default function useSearchFerry() {
-  const [resultTrips, setResultTrips] = useState([])
-  const [loading, setloading] = useState(false)
-  const [emptyResult, setEmptyResult] = useState(false)
+  const [resultTrips, setResultTrips] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [emptyResult, setEmptyResult] = useState(false);
 
-  const searchFerry = ({currentRoute, currentDate, currentPassengers}) => {
-    const departuresURL = `https://tadpole.clickferry.app/departures?route=${currentRoute.code}&time=${currentDate}`
+  const searchFerry = async ({ currentRoute, currentDate, currentPassengers }) => {
+    const departuresURL = `https://tadpole.clickferry.app/departures?route=${currentRoute.code}&time=${currentDate}`;
 
-    setloading(true)
-    setEmptyResult(false)
-    setResultTrips([])
+    setLoading(true);
+    setEmptyResult(false);
+    setResultTrips([]);
 
-    fetch(departuresURL)
-      .then((res) => res.json())
-      .then((departures) => {
-        departures.forEach((departure) => {
-          const departureDate = departure.time.split("Z")[0]
-          const accommodationURL = `https://tadpole.clickferry.app/accommodations?route=${currentRoute.code}&time=${departureDate}&adults=${currentPassengers.adults}&children=${currentPassengers.children}&babies=${currentPassengers.babies}`
+    try {
+      const departuresResponse = await fetch(departuresURL);
+      const departures = await departuresResponse.json();
 
-          fetch(accommodationURL)
-            .then((res) => res.json())
-            .then((accommodations) => {
-              accommodations.forEach((accommodation, index, arr) => {
-                const priceURL = `https://tadpole.clickferry.app/price?route=${currentRoute.code}&time=${departureDate}&adults=${currentPassengers.adults}&children=${currentPassengers.children}&babies=${currentPassengers.babies}&accommodation=${accommodation.code}`
-                fetch(priceURL)
-                  .then((res) => res.json())
-                  .then((price) => {
-                    const resultTrip = {
-                      ...departure,
-                      ...accommodation,
-                      ...price,
-                      "from": currentRoute.from,
-                      "to": currentRoute.to
-                    }
-                    console.log(resultTrip)
-                    setResultTrips((resultTrips) => [...resultTrips, resultTrip])
-                  })
-              })
-            })
-        })
-      }).finally(() => {
-        setTimeout(() => {
-          setloading(false)
-          setEmptyResult(true)
-        }, 600)
-      })
-  }
+      departures.forEach(async (departure) => {
+        const departureDate = departure.time.split("Z")[0];
+        const accommodationURL = `https://tadpole.clickferry.app/accommodations?route=${currentRoute.code}&time=${departureDate}&adults=${currentPassengers.adults}&children=${currentPassengers.children}&babies=${currentPassengers.babies}`;
 
+        const accommodationResponse = await fetch(accommodationURL);
+        const accommodations = await accommodationResponse.json();
 
+        accommodations.forEach(async (accommodation) => {
+          const priceURL = `https://tadpole.clickferry.app/price?route=${currentRoute.code}&time=${departureDate}&adults=${currentPassengers.adults}&children=${currentPassengers.children}&babies=${currentPassengers.babies}&accommodation=${accommodation.code}`;
 
-  return [resultTrips, searchFerry, loading, emptyResult]
+          const priceResponse = await fetch(priceURL);
+          const price = await priceResponse.json();
+
+          const resultTrip = {
+            ...departure,
+            ...accommodation,
+            ...price,
+            from: currentRoute.from,
+            to: currentRoute.to,
+          };
+
+          setResultTrips((resultTrips) => [...resultTrips, resultTrip]);
+        });
+      });
+
+    } catch (error) {
+      console.error("Error:", error);
+    } finally {
+      setTimeout(() => {
+        setLoading(false);
+        setEmptyResult(true);
+      }, 600);
+    }
+  };
+
+  return [resultTrips, searchFerry, loading, emptyResult];
 }
